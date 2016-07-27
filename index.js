827c43ec33ed8481c3e7b4999755e09bd2efe822
dYb <duanyubin2012@gmail.com>
2016-07-27 18:25:28 +0800

    Add some interfaces

---
+++ index.js
@@ -0,0 +1,6 @@
+import NEWSAPPAPI from './lib'
+
+if (typeof exports !== 'object' && typeof define !== 'function') {
+  window.NEWSAPPAPI = NEWSAPPAPI
+}
+export default NEWSAPPAPI
--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,14 @@ let pkg = require('./package.json');
 let external = Object.keys(pkg.dependencies);
 
 export default {
-  entry: 'lib/index.js',
+  entry: './index.js',
   plugins: [babel(babelrc())],
   external: external,
   targets: [
     {
       dest: pkg['main'],
       format: 'umd',
-      moduleName: 'rollupStarterProject',
+      moduleName: 'newsapp',
       sourceMap: true
     },
     {
